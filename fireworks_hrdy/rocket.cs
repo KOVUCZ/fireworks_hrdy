ca1c771c284bd12bb7f53f884269c85f14ea44e7
KOVUCZ <Hrdy.Adam@stud.vassboskovice.cz>
2023-11-29 12:26:44 +0100

    speedvector

--- fireworks_hrdy/rocket.cs
+++ fireworks_hrdy/rocket.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace fireworks_hrdy
 {
     internal class rocket
     {
-         
+        public Vector gravity { get; } = new Vector(0, 10); 
+        public Point Location { get; set; }
+        public Vector SpeedVector { get; set; }
+
+
+
+        public void Move()
+        {
+            Location.Offset(SpeedVector.X, SpeedVector.Y);
+            SpeedVector = Vector.Add(SpeedVector, gravity);
+        }
+
+        public void Explode()
+        {
+
+        }
     }
 }
